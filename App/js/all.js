73876590c0136cdeebc6fe035d923a42fb061e14
Dobriy78 <robe@bk.ru>
2012-12-11 18:57:48 +0400

    s

--- App/js/all.js
+++ App/js/all.js
@@ -144,3 +144,93 @@ $(document).ready(function(){
 });
 };
 */
+
+// свитчеры //
+
+$(document).ready( function(){ 
+    $("#lr1").click(function(){
+        var parent = $(this).parents('.switch');
+        $('#lr2',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', true);
+    });
+    $("#lr2").click(function(){
+        var parent = $(this).parents('.switch');
+        $('#lr1',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', false);
+    });
+    $("#lr3").click(function(){
+        var parent = $(this).parents('.switch');
+        $('#lr4',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', true);
+    });
+    $("#lr4").click(function(){
+        var parent = $(this).parents('.switch');
+        $('#lr3',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', false);
+    });
+});
+
+// Звгрузка дефолта
+
+$('#radio2').click(function(){
+$(".textTTH").removeClass("hidden");
+$(".textTTTH").removeClass("hidden");
+});
+$('#radio1').click(function(){
+$(".textTTH").addClass("hidden");
+$(".textTTTH").addClass("hidden");
+// загрузка по дефолту
+//var assa = location.pathname;
+//document.write(assa);
+var pathName = location.pathname.replace(/\/C/g,"C").replace(/\//g,"\\").replace(/index.html/g,"Tester.config");
+//document.write(pathName);
+//function useDefaultSettings()
+//{
+//alert(y);
+$(document).ready(function(){
+	$.ajax({
+		type: "GET",
+		url: pathName,
+		dataType: "config",
+		success: function(xml) {
+			$(xml).find('userSettings').each(function(){
+				var id = $(this).attr('id');
+				var jarPath = $(this).find('TesterJarPath').text().replace(/\\/g,"\\");
+				    console.log('TesterJarPath:			' + jarPath);
+				var gwPath = $(this).find('TesterGWPath').text().replace(/\\/g,"\\");
+				    console.log('TesterGWPath:			' + gwPath);
+				var scenarioPath = $(this).find('TesterScenarioPath').text().replace(/\\/g,"\\");
+				    console.log('TesterScenarioPath:		' + scenarioPath);
+				//$('<div class="items"></div>').html('<h2 class=\'small\'>'+name+'</h2><pre>'+desc+'</pre>').appendTo('#scenarioDescription');
+			});
+		}
+	});
+});
+});
+
+/*
+var fs = require("fs"),
+    sys = require("sys");
+var testFile = location.pathname.replace(/\/C/g,"C").replace(/index.html/g,"test.txt");
+fs.open( testFile, "r+", 0644, function(err, file_handle) {
+if (!err) {
+    alert("Read" + testFile)
+	fs.write(file_handle, "Copyrighted by Me", null, 'ascii', function(err, written) {
+        if (!err) {
+            // Всё прошло хорошо
+        } else {
+            // Произошла ошибка при записи
+        }
+    });
+} else {
+    alert("Not read")
+}
+});
+*/
+
+// Показать скрыть настройки //
+
