7379cc7a56329f6684de70295c98da4ffccf09b5
Dobriy78 <robe@bk.ru>
2012-12-17 23:31:57 +0400

    1

--- App/js/config.js
+++ App/js/config.js
@@ -1,55 +1,55 @@
-var pPath = "C:\\1 1\\tester.config";
+var pPath = "..\\..\\GW-TESTER\\tester.config";
 var fs = require("fs"),
     sys = require("sys");
 	
 $(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url: pPath,
-		dataType: "xml",
-		success: function(xml) {
-			$(xml).find('UserSettings').each(function(){
-				var id = $(this).attr('id');
-				var UseDefault = $(this).find('UseDefault').text();;
-				    console.log('UseDefault:			' + UseDefault);
-				var JarPath = $(this).find('JarPath').text();;
-				    console.log('JarPath:			' + JarPath);
-				var GWPath = $(this).find('GWPath').text();
-				    console.log('GWPath: 			' + GWPath);
-				var ScenarioPath = $(this).find('ScenarioPath').text();
-				    console.log('ScenarioPath: 		' + ScenarioPath);
+   $.ajax({
+   type: "GET",
+   url: pPath,
+   dataType: "xml",
+ success: function(xml) {
+   $(xml).find('UserSettings').each(function(){
+   var id = $(this).attr('id');
+   var UseDefault = $(this).find('UseDefault').text();;
+   console.log('UseDefault:			' + UseDefault);
+   var JarPath = $(this).find('JarPath').text();;
+   console.log('JarPath:			' + JarPath);
+   var GWPath = $(this).find('GWPath').text();
+   console.log('GWPath: 			' + GWPath);
+   var ScenarioPath = $(this).find('ScenarioPath').text();
+   console.log('ScenarioPath: 		' + ScenarioPath);
 					
 if ( UseDefault == "Y" ) {
-	$('#default').attr('checked', true);
-	$(".textTTH").addClass("hidden");
-	$(".textTTTH").addClass("hidden");
+   $('#default').attr('checked', true);
+   $(".textTTH").addClass("hidden");
+   $(".textTTTH").addClass("hidden");
 };
 if ( UseDefault == "N" ) {
-	$('#default').attr('checked', false);
-	$(".textTTH").removeClass("hidden");
-	$(".textTTTH").removeClass("hidden");
-};
-
-			});
-		}
+   $('#default').attr('checked', false);
+   $(".textTTH").removeClass("hidden");
+   $(".textTTTH").removeClass("hidden");
+};});},
+ error: function(){ 
+   alert("NOT exists");  //Р·РґРµСЃСЊ РґРѕР±Р°РІРёС‚СЊ РѕР±СЂР°Р±РѕС‚РєСѓ С„РѕСЂРјС‹ РЅР° Р·Р°РїРёСЃСЊ
+} 
 	});
 });
 
+/* record neq config
 var configNew = "<UserSettings>\n<UseDefault>Y</UseDefault>\n<JarPath>C:\\Tester\\Tester.jar</JarPath>\n<GWPath>C:\\Tester\\TesterGW.xml</GWPath>\n<ScenarioPath>C:\\Tester\\Scenarios\\</ScenarioPath>\n</UserSettings>";
 
-fs.open("C:\\1 1\\tester.config", "w", 0644, function(err, file_handle) {
+fs.open("..\\..\\GW-TESTER\\tester.config", "w", 0644, function(err, file_handle) {
 if (!err) {
-    // Записываем в конец файла readme.txt фразу "Copyrighted by Me"
-    // при открытии в режиме "a" указатель уже в конце файла, и мы передаём null
-    // в качестве позиции
     fs.write(file_handle, configNew, null, 'utf8', function(err, written) {
         if (!err) {
-            // Всё прошло хорошо
+
         } else {
-            // Произошла ошибка при записи
+
         }
     });
 } else {
-    // Обработка ошибок при открытии
+
 }
 });
+
+*/
